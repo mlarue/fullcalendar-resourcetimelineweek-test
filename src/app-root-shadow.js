907977d2ce48695cfab707a65e4907fcb94556b7
mlarue <mlarue@usermode.de>
2020-10-07 14:46:15 +0200

    initial

---
+++ src/app-root-shadow.js
@@ -0,0 +1,36 @@
+import { Calendar } from '@fullcalendar/core';
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
+
+import commonStyles from '@fullcalendar/common/main.css';
+import dayGridStyles from '@fullcalendar/daygrid/main.css';
+import timelineStyles from '@fullcalendar/timeline/main.css';
+import resourceTimelineStyles from '@fullcalendar/resource-timeline/main.css';
+
+export class AppRoot extends HTMLElement {
+
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({mode: 'open'});
+        shadowRoot.innerHTML = `
+            <style>${commonStyles}</style>
+            <style>${dayGridStyles}</style>    
+            <style>${timelineStyles}</style>
+            <style>${resourceTimelineStyles}</style>
+            <style>
+                #calendar {
+                    width: 50%;
+                }
+            </style>
+            <div id="calendar"></div>
+        `;
+
+        const calendarEl = shadowRoot.getElementById('calendar');
+        const calendar = new Calendar(calendarEl, {
+            plugins: [resourceTimelinePlugin],
+            initialView: 'resourceTimelineWeek'
+        });
+
+        calendar.render();
+    }
+}
+
+customElements.define("app-root", AppRoot);
